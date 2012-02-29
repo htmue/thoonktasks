c20ea558febe8aa8ae275beb02901947e26c30eb
Hans-Thomas Mueller <htmue@mac.com>
2012-02-29 18:44:36 +0100

    Prepare for github.

---
+++ README.md
@@ -0,0 +1,14 @@
+Thoonktasks: Tasks on top of Thoonk.py
+======================================
+
+See `examples` for examples.
+
+Proof of concept implementation, alpha status.
+
+
+License
+-------
+ 
+This is free and unencumbered software released into the public domain.
+ 
+see [UNLICENSE](http://unlicense.org/)
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# Created by Hans-Thomas on 2012-02-29.
+#=============================================================================
+#   setup.py --- 
+#=============================================================================
+from distutils.core import setup
+
+
+setup(
+    name='thoonktasks',
+    version='0.1.0',
+    description='Tasks on top of Thoonk.py',
+    author='Hans-Thomas Mueller',
+    author_email='htmue@mac.com',
+    url='https://https://github.com/htmue/thoonktasks',
+    packages=['thoonktasks'],
+)
+
+#.............................................................................
+#   setup.py
